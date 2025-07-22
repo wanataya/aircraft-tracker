791a7526519a2a4d624d78e8fe474fc62938f5d1
Mochammad Alwan Al Ataya <122243432+wanataya@users.noreply.github.com>
2025-07-22 10:09:22 +0700

    fix: tailwind.css (#2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-07-22',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
 })
